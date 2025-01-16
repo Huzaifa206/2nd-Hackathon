d16c0d1e7a2e4f604de1d2e1815be1e856bc23a4
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-01-17 02:06:57 +0500

    .

--- app/components/Essentials.tsx
+++ app/components/Essentials.tsx
@@ -5,7 +5,7 @@ export default function Essential() {
       <div>
         <h2 className="font-semibold text-xl py-5"> The Essentials</h2>
       </div>
-      <div className="flex sm:flex-col md:flex-row justify-evenly ">
+      <div className="flex xs:flex-col md:flex-row justify-evenly ">
       <Image width={350} height={300} alt="" src={"/Frame.png"} />
       <Image width={350} height={300} alt="" src={"/Frame1.png"} />
       <Image width={350} height={300} alt="" src={"/Frame2.png"} />

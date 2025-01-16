a13a01c4d8d3fdc9d4171e2ae2fefa977ae47834
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-01-17 02:05:43 +0500

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,21 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'xs':'480px',
+        'sm':'640px',
+        'md':'768px',
+        'lg':'1024px',
+        'xl':'1280px',
+        '2xl':'1536px',
+        'custom':'1400px',
+  
+      },
+      borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)'
+        }
     },
   },
   plugins: [],

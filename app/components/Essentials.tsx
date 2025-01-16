d6bdb50dbb7821620e9b8169ecd6cfec72f423e2
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-01-17 01:44:46 +0500

    .

--- app/components/Essentials.tsx
+++ app/components/Essentials.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 export default function Essential() {
   return (
-    <section className="w-screen m-4" >
+    <section className="w-screen p-4" >
       <div>
         <h2 className="font-semibold text-xl py-5"> The Essentials</h2>
       </div>
       <div className="flex justify-evenly ">
-        <Image width={350} height={300} alt="" src={"/Frame.png"} />
-        <Image width={350} height={300} alt="" src={"/Frame1.png"} />
-        <Image width={350} height={300} alt="" src={"/Frame2.png"} />
+      
       </div>
     </section>
   );

6cd4c426ccf532fb85b07e981fd557ff27838a99
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-01-17 03:05:56 +0500

    .

--- app/components/Essentials.tsx
+++ app/components/Essentials.tsx
@@ -5,10 +5,10 @@ export default function Essential() {
       <div>
         <h2 className="font-semibold text-xl py-5"> The Essentials</h2>
       </div>
-      <div className="flex xs:flex-col md:flex-row justify-evenly ">
-      <Image width={350} height={300} alt="" src={"/Frame.png"} />
-      <Image width={350} height={300} alt="" src={"/Frame1.png"} />
-      <Image width={350} height={300} alt="" src={"/Frame2.png"} />
+      <div className="flex flex-col md:flex-row justify-evenly ">
+      <Image width={350} height={300} alt="" src={"/Frame.png"} layout="responsive"/>
+      <Image width={350} height={300} alt="" src={"/Frame1.png"} layout="responsive"/>
+      <Image width={350} height={300} alt="" src={"/Frame2.png"} layout="responsive"/>
       </div>
     </section>
   );
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
     return (
       <header className="w-screen flex justify-between items-center p-[10px_40px] bg-white shadow-md">
         <div className="bg-cover w-[7%]"><img src="/logonike.png" alt="Nike" /></div>
-        <nav>
-          <ul className="flex list-none gap-5">
+        
+        <ul className="hidden md:flex list-none gap-5">
             <li className="cursor-pointer transition-colors duration-300 hover:text-red-500">
               <Link href="/">New & Featured</Link>
             </li>
@@ -28,16 +28,16 @@ export default function Header() {
             <li className="cursor-pointer transition-colors duration-300 hover:text-red-500">
               <Link href="/">SNKRS</Link>
             </li>
-          </ul>
-        </nav>
+        </ul>
+        
   
         <div className="flex items-center justify-between gap-4">
           <div className="flex rounded-3xl px-2 py-1 w-[80%] border-2 border-text-secondary-gray gap-2 bg-gray-100">
              <img src="/search.svg" alt="wishlist" />
-             <input className="focus-visible:outline-none w-[64%] bg-gray-100" type="text" placeholder="Search" />
+             <input className="hidden md:block focus-visible:outline-none w-[64%] bg-gray-100" type="text" placeholder="Search" />
           </div>
-          <div className="wishlist"><img src="/wishlist.svg" alt="wishlist" /></div>
-          <div className="cart"><img src="/cart.svg" alt="cart" /></div>
+          <div className="wishlist"><Image src={"/wishlist.svg"} width={800} height={60} classname alt="" layout="responsive" /></div>
+          <div className="cart"><Image src={"/cart.svg"} width={800} height={600} alt="" layout="responsive" /></div>
         </div>
         
       </header>

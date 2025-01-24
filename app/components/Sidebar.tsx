0e9fb0e6062e6f0f2b7949dbd2e5bc14978d7e15
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-01-24 23:00:45 +0500

    .

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,14 +4,33 @@ import { AiFillProduct } from "react-icons/ai";
 import { IoHelp } from "react-icons/io5";
 import { IoMenu } from "react-icons/io5";
 import Link from "next/link";
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 export default function Sidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+
  
    const toggleSidebar = () => {
      setIsSidebarOpen(!isSidebarOpen);
    };
+
+   useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(e.target as Node)) {
+        setIsSidebarOpen(false);
+      }
+    };
+  
+    if (isSidebarOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isSidebarOpen]);
+  
 
   return (
     <div>
@@ -21,6 +40,7 @@ export default function Sidebar() {
            <div className="md:hidden "><IoMenu className="flex justify-center items-center size-6 cursor-pointer transition-colors duration-300 hover:text-red-500" /></div>
         </button>
         <div
+         ref={sidebarRef}
           className={`z-50 fixed top-0 right-0 h-screen w-64 bg-[#666666] text-white transform transition-transform ${
             isSidebarOpen ? 'translate-x-0' : 'translate-x-full'
           }`}

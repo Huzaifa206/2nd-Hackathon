705e2cc1f68590e10f6d00b0efb63de7820a064b
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-02-06 00:18:35 +0500

    .

--- app/Join-us/page.tsx
+++ app/Join-us/page.tsx
@@ -8,24 +8,24 @@ export default function JoinUs() {
            <h2 className="uppercase text-center font-bold pb-5"> BECOME A NIKE MEMBER </h2>
            <p className='w-80 text-center text-text-secondary-gray pb-5'>Create your Nike Member profile and get first access to the very best of Nike products, inspiration and community.</p>
            <form action="" className="flex flex-col mx-auto w-96 my-2">
-             <input className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Email Address" />
-             <input className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Password" />
-             <input className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="First Name" />
-             <input className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Last Name" />
-             <input className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-2 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Date of Birth" />
-             <p className='pb-3 text-text-secondary-gray text-sm text-center'>Get a Nike Member Reward every year on your Birthday.</p>
-             <select className="w-80 md:w-96 border-[#E5E5E5] rounded-md mb-2 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" name="" id="">
+             <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Email Address" />
+             <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Password" />
+             <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="First Name" />
+             <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Last Name" />
+             <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-2 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Date of Birth" />
+             <p className='mx-auto pb-3 text-text-secondary-gray text-sm text-center'>Get a Nike Member Reward every year on your Birthday.</p>
+             <select className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-2 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" name="" id="">
                <option className='text-text-secondary-gray' value="">India</option>
                <option className='text-text-secondary-gray' value="">United States</option>
                <option className='text-text-secondary-gray' value="">United Kingdom</option>
                <option className='text-text-secondary-gray' value="">Pakistan</option>
               <option className='text-text-secondary-gray' value="">Canada</option>
              </select>
-      <div className='flex gap-5 mt-1'>
+      <div className='flex gap-5 mt-1 mx-auto w-80 md:w-96'>
         <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-2/4 text-center text-text-secondary-gray'>Male</div>
         <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-2/4 text-center text-text-secondary-gray'>Female</div>
       </div>
-      <div className="flex justify-between items-center py-7">
+      <div className="flex justify-between items-center py-7 mx-auto w-80 md:w-96">
         <div>
           <input type="checkbox" id="remember" />
           <label className="ml-2 pl-2 cursor-pointer text-text-secondary-gray" htmlFor="remember">
@@ -33,8 +33,8 @@ export default function JoinUs() {
           </label>
         </div>
       </div>
-      <p className="text-text-secondary-gray text-center pb-6">By creating an account, you agree to Nike's Privacy Policy and Terms of Use.</p>
-      <button className="rounded-sm py-3 px-24 bg-black text-white mb-2 hover:bg-slate-700">Sign Up</button>
+      <p className="mx-auto w-80 md:w-96 text-text-secondary-gray text-center pb-6">By creating an account, you agree to Nike's Privacy Policy and Terms of Use.</p>
+      <button className="mx-auto w-80 md:w-96 rounded-sm py-3 bg-black text-white mb-2 hover:bg-slate-700">Sign Up</button>
       <p className="text-center"> <span className="text-text-secondary-gray">Already a Member? </span><Link href="/Login" className="flex justify-center font-bold cursor-pointer transition-colors duration-300 hover:text-red-500 underline" >Sign In</Link></p>
     </form>
     </div>

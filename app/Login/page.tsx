3e78f48ddfd3b4e1d14bb884563f42b78fd6e342
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-02-05 23:59:22 +0500

    .

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -8,7 +8,7 @@ export default function Login() {
         <h2 className="uppercase w-40 text-center font-bold pb-5">
           YOUR ACCOUNT FOR EVERYTHING NIKE
         </h2>
-        <form action="" className="flex flex-col mx-auto w-96 my-5 md:my-0">
+        <form action="" className="flex flex-col mx-auto my-0">
           <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Email Address" />
           <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Password" />
           <div className="flex justify-between items-center py-3 flex-col md:flex-row gap-2">
@@ -24,9 +24,9 @@ export default function Login() {
               </span>
             </div>
           </div>
-          <p className="text-text-secondary-gray text-center pb-6 text-sm md:text-base">By logging in, you agree to Nike's <span> Privacy Policy</span> and <span>Terms of Use</span>.
+          <p className="text-text-secondary-gray text-center pb-6 text-sm md:text-base mx-auto">By logging in, you agree to Nike's <span> Privacy Policy</span> and <span>Terms of Use</span>.
          </p>
-          <button className="rounded-sm py-3 bg-black mb-4 px-24 text-white text-center  hover:bg-slate-700">Sign in</button> 
+          <button className="rounded-sm mx-auto w-80 md:w-96 py-3 bg-black mb-4 text-white text-center  hover:bg-slate-700">Sign in</button> 
           <p className="text-center"> <span className="text-text-secondary-gray">Not a Member? </span></p>
         
         </form>

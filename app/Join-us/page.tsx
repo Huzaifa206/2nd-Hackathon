bea89c0012dfb3778e6aa57e9ba925af1676f4cd
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2025-02-06 00:23:29 +0500

    .

--- app/Join-us/page.tsx
+++ app/Join-us/page.tsx
@@ -7,7 +7,7 @@ export default function JoinUs() {
            <Image className="pb-5" src="/TickLogo.png" width={40} height={10} alt="Logo" />
            <h2 className="uppercase text-center font-bold pb-5"> BECOME A NIKE MEMBER </h2>
            <p className='w-80 text-center text-text-secondary-gray pb-5'>Create your Nike Member profile and get first access to the very best of Nike products, inspiration and community.</p>
-           <form action="" className="flex flex-col mx-auto w-96 my-2">
+           <form action="" className="flex flex-col mx-auto w-96">
              <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Email Address" />
              <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="Password" />
              <input className="mx-auto w-80 md:w-96 border-[#E5E5E5] rounded-md mb-4 px-4 py-3 border-[2px] placeholder:text-text-secondary-gray" type="text" placeholder="First Name" />
@@ -22,10 +22,10 @@ export default function JoinUs() {
               <option className='text-text-secondary-gray' value="">Canada</option>
              </select>
       <div className='flex gap-5 mt-1 mx-auto w-80 md:w-96'>
-        <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-2/4 text-center text-text-secondary-gray'>Male</div>
-        <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-2/4 text-center text-text-secondary-gray'>Female</div>
+        <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-[50%] text-center text-text-secondary-gray'>Male</div>
+        <div className='border-[#E5E5E5] rounded-md py-4 border-[2px] w-[50%] text-center text-text-secondary-gray'>Female</div>
       </div>
-      <div className="flex justify-between items-center py-7 mx-auto w-80 md:w-96">
+      <div className="flex justify-between items-center py-7 mx-auto text-sm md:text-base">
         <div>
           <input type="checkbox" id="remember" />
           <label className="ml-2 pl-2 cursor-pointer text-text-secondary-gray" htmlFor="remember">
@@ -33,7 +33,7 @@ export default function JoinUs() {
           </label>
         </div>
       </div>
-      <p className="mx-auto w-80 md:w-96 text-text-secondary-gray text-center pb-6">By creating an account, you agree to Nike's Privacy Policy and Terms of Use.</p>
+      <p className="mx-auto text-sm md:text-base text-text-secondary-gray text-center pb-6">By creating an account, you agree to Nike's Privacy Policy and Terms of Use.</p>
       <button className="mx-auto w-80 md:w-96 rounded-sm py-3 bg-black text-white mb-2 hover:bg-slate-700">Sign Up</button>
       <p className="text-center"> <span className="text-text-secondary-gray">Already a Member? </span><Link href="/Login" className="flex justify-center font-bold cursor-pointer transition-colors duration-300 hover:text-red-500 underline" >Sign In</Link></p>
     </form>
